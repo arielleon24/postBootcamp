3a548acf4c1fca1d52f2ace520e6b2400f7cdfaf
Ramon Ariel Leon Flores <arielleon24@gmail.com>
2021-02-04 23:59:47 -0500

    function implemented - not refactored

--- loopy_refactor/loopyRefact.js
+++ loopy_refactor/loopyRefact.js
@@ -1,4 +1,13 @@
 const loopyLighthouse = (range, multiples, words) => {
+  for (let i = range[0]; i < range[1]; i++) {
+    if(i % multiples[0] === 0) {
+      console.log(words[0])
+    } else if (i % multiples[1] === 0) {
+      console.log(words[1])
+    } else {
+      console.log(i)
+    }
+  }
   
 };
 
